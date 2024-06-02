f981cc2124eca02aa2dd608c55230251633e9637
Sergey <masewl@gmail.com>
2024-06-02 17:36:57 +0300

    update game_v3

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -1,4 +1,4 @@
-def game_core_v3(number: int = 1) -> int:
+def guesser(number: int = 1) -> int:
     """Угадываем число методом деления интервалов на 2 две равные части 
     (первым загадываем 50, потом либо 25 либо 75 и так далее...)
        Функция принимает загаданное число и возвращает число попыток
@@ -10,7 +10,7 @@ def game_core_v3(number: int = 1) -> int:
     """
     # Ваш код начинается здесь
     count = 0
-    interval = [1, 101]
+    interval = (1, 101)
     while True:
         count += 1
         predict = sum(interval) // 2
@@ -25,3 +25,24 @@ def game_core_v3(number: int = 1) -> int:
     # Ваш код заканчивается здесь
 
     return count
+def guessing_statistics(guesser) -> dict:
+    """Проводит анализ за сколько раз угадываются числа от 1 до 100
+
+    Args:
+        guesser ([type]): функция угадывания    
+
+    Returns:
+        dict: Keys: количество угадываний; Values: количество чисел из диапазона от 1 до 100
+    """
+    count_dict = {} # словарь для хранения сохранения количества попыток
+    temp_value = 0 # временная переменная количесвта попыток на текущее число
+    for i in range(1,101):
+        temp_value = guesser(i)
+        if not temp_value in count_dict: # если текущего эл. нет в  словаре
+            count_dict[temp_value] = 1
+        else:
+            count_dict[temp_value] += 1 # добавляем ему значение +1
+            
+    return(dict(sorted(count_dict.items())))
+    
+print (guessing_statistics(guesser))

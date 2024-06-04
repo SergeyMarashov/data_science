251cd38049563425bd3ba32c36584d34a70b089d
Sergey <masewl@gmail.com>
2024-06-04 20:01:23 +0300

    final processing

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def guesser(number: int = 1) -> int:
     """Угадываем число методом деления интервалов на 2 две равные части 
     (первым загадываем 50, потом либо 25 либо 75 и так далее...)
@@ -8,7 +10,6 @@ def guesser(number: int = 1) -> int:
     Returns:
         int: Число попыток
     """
-    # Ваш код начинается здесь
     count = 0
     interval = (1, 101)
     while True:
@@ -21,28 +22,24 @@ def guesser(number: int = 1) -> int:
         elif number < predict:
             interval = (interval[0], predict)
 
-
-    # Ваш код заканчивается здесь
-
     return count
-def guessing_statistics(guesser) -> dict:
-    """Проводит анализ за сколько раз угадываются числа от 1 до 100
 
-    Args:
-        guesser ([type]): функция угадывания    
-
-    Returns:
-        dict: Keys: количество угадываний; Values: количество чисел из диапазона от 1 до 100
+def cocollecting_statistics(loocking_number):
+    """ Функция для сбора статистики расчетов нашего когда.
+        Принимает функцию, выдает сообщение о максимальном, минимальном и среднем
+        количестве результатов выполнения нашего кода за 1000 запусков
     """
-    count_dict = {} # словарь для хранения сохранения количества попыток
-    temp_value = 0 # временная переменная количесвта попыток на текущее число
-    for i in range(1,101):
-        temp_value = guesser(i)
-        if not temp_value in count_dict: # если текущего эл. нет в  словаре
-            count_dict[temp_value] = 1
-        else:
-            count_dict[temp_value] += 1 # добавляем ему значение +1
-            
-    return(dict(sorted(count_dict.items())))
     
-print (guessing_statistics(guesser))
+    count_lst = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=(1000))
+    
+    for number in random_array:   
+        count_lst.append(loocking_number(number))
+    
+    print(f'Среднее число попыток {int(np.mean(count_lst))}') # Ожидаемо 6
+    print(f'Максимальное количество попыток {max(count_lst)}') # Ожидаемо 8
+    print(f'Минимальное количество попыток {min(count_lst)}') # Ожидаемо 1
+    
+if __name__ == "__main__":
+    cocollecting_statistics(guesser)
